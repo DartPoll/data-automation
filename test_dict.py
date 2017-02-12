2b19275a81041dab1a1a684b81a46b709c8b236f
Mike Zhu <mikezhu@mikes-air.wireless.dartmouth.edu>
2017-02-12 00:03:59 -0500

    Test dictionary

---
+++ test_dict.py
@@ -0,0 +1,33 @@
+survey = {"Total": {"Total": 1000},
+          "Question1": {"A": {"Total": 100,
+                              "Race": {
+                                  "White": 25,
+                                  "Black": 75
+                              },
+                              "Gender": {
+                                  "Male": 30,
+                                  "Female": 70
+                              }
+                              },
+                        "B": {"Total": 100,
+                              "Race": {
+                                  "White": 20,
+                                  "Black": 80
+                              },
+                              "Gender": {
+                                  "Male": 70,
+                                  "Female": 30
+                              }
+                              }
+                        },
+          "Question2": {"A": {"Total": 100,
+                              "Race": {
+                                  "White": 10,
+                                  "Black": 90},
+                              "Gender": {
+                                  "Male": 50,
+                                  "Female": 50
+                              }
+                              }
+                              }
+          }
